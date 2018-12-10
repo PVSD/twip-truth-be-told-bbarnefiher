1af10e5a231bee00286ee8db3f87f444007c2e48
bbarnefiher <bb222@pvsd.org>
2018-12-10 11:20:45 -0500

    Brain and Winston's Jawnski

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
-
+import java.util.*;
+import java.io.*;
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException{
+
+        File file = new File("TruthTable.txt");
+        FileWriter writer = new FileWriter(file);
+        writer.write("Hi Brain");
+        writer.close();
+        Scanner userInput = new Scanner(System.in);
     }
 }
